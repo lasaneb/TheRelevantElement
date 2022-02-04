ceef3031d4297efff104bbc7895829fdb2522257
Airmanf7 <88789678+Airmanf7@users.noreply.github.com>
2022-02-04 17:37:22 -0500

    updates to wombo art

--- .py_files/apps/wombo_art_using_helium_driver.py
+++ .py_files/apps/wombo_art_using_helium_driver.py
@@ -1,7 +1,7 @@
 #Initial Imports
 
-import helium 
-from helium import *
+import helium as he
+# from helium import *
 import shutil
 import time
 from selenium import webdriver
@@ -9,18 +9,18 @@ from selenium import webdriver
 # Function to get wombo art
 def create_wombo_art(title, art_style):
 
-    helium.start_chrome('https://app.wombo.art')
-    helium.write(title, into="Type anything")
-    helium.click(Image(alt = art_style))
-    helium.click("Create")
-    helium.wait_until(Text("Name artwork").exists,timeout_secs=60)
+    he.start_chrome('https://app.wombo.art')
+    he.write(title, into="Type anything")
+    he.click(Image(alt = art_style))
+    he.click("Create")
+    he.wait_until(Text("Name artwork").exists,timeout_secs=60)
     title = title.replace(" " , "_") 
-    helium.write(title, into="Enter title")
-    helium.click("Save")
+    he.write(title, into="Enter title")
+    he.click("Save")
     time.sleep(10)
     original = r"C://Users//Airma//Downloads//" + title + "_TradingCard.jpg"
     target = r"C://Users//Airma//FinTechClass//MiamiClasswork//other_projects//relevant_element//resources//Images//" + title + ".jpg"
     shutil.move(original,target)
-    helium.kill_browser()
+    he.kill_browser()
     
     return title
