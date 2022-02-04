5ed34ba9e337a9d35c71fc5ed8bdb3ec864d91af
Airmanf7 <88789678+Airmanf7@users.noreply.github.com>
2022-02-04 17:14:42 -0500

    updates

--- .py_files/the_relevant_element.py
+++ .py_files/the_relevant_element.py
@@ -1,6 +1,18 @@
-
-# Streamlit Interface
-
 import streamlit as st
-from dataclasses import dataclass
-from typing import Any, List
+
+from multiapp import MultiApp
+
+from apps import home, data # import your app modules here
+
+relevant_element = MultiApp()
+
+st.markdown("Some Text Here")
+
+# Add all your applications here as tabs
+
+relevant_element.add_app("Home", home.app)
+relevant_element.add_app("Data", data.app)
+
+
+# The main app
+relevant_element.run()
