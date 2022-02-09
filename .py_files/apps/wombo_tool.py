59d000087e3d46ffd29b693955ee860fb0846b50
Airmanf7 <88789678+Airmanf7@users.noreply.github.com>
2022-02-08 23:30:11 -0500

    updates

--- .py_files/apps/wombo_tool.py
+++ .py_files/apps/wombo_tool.py
@@ -1,18 +1,13 @@
 import requests, os
 from colorama import Fore, init
-import streamlit as st
 
 init(autoreset=True)
 
-if st.button("Get Art"):
+def generate(title: str):
+    os.system("cls")
+    print(f"{Fore.LIGHTBLUE_EX}Retrieving Auth Token.")
 
-    
-
-    def generate(title: str):
-        os.system("cls")
-        print(f"{Fore.LIGHTBLUE_EX}Retrieving Auth Token.")
-
-        getAuthToken = requests.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCvp5MTJLUdtBYEKYWXJrlLzu1zuKM6Xw", headers={
+    getAuthToken = requests.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCvp5MTJLUdtBYEKYWXJrlLzu1zuKM6Xw", headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0",
         "Accept": "*/*",
         "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -26,16 +21,16 @@ if st.button("Get Art"):
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "cross-site",
         "TE": "trailers"
-        }, json={
-            "returnSecureToken": "true"
-        }).json()
+    }, json={
+        "returnSecureToken": "true"
+    }).json()
 
-        authToken = getAuthToken["idToken"]
+    authToken = getAuthToken["idToken"]
 
-        os.system("cls")
-        print(f"{Fore.LIGHTBLUE_EX}Initiating task.")
+    os.system("cls")
+    print(f"{Fore.LIGHTBLUE_EX}Initiating task.")
 
-        initTask = requests.post("https://app.wombo.art/api/tasks", headers={
+    initTask = requests.post("https://app.wombo.art/api/tasks", headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0",
         "Accept": "*/*",
         "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -49,16 +44,16 @@ if st.button("Get Art"):
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-origin",
         "TE": "trailers"
-        }, json={
+    }, json={
         "premium": False
-        }).json()
+    }).json()
 
-        taskID = initTask["id"]
+    taskID = initTask["id"]
 
-        os.system("cls")
-        print(f"{Fore.LIGHTBLUE_EX}Initiating art creation.")
+    os.system("cls")
+    print(f"{Fore.LIGHTBLUE_EX}Initiating art creation.")
 
-        initCreateTask = requests.put(f"https://app.wombo.art/api/tasks/{taskID}", headers={
+    initCreateTask = requests.put(f"https://app.wombo.art/api/tasks/{taskID}", headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0",
         "Accept": "*/*",
         "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -74,7 +69,7 @@ if st.button("Get Art"):
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-origin",
         "TE": "trailers"
-        }, json={
+    }, json={
         "input_spec": {
             "prompt": title,
             "style": 18,
@@ -82,13 +77,13 @@ if st.button("Get Art"):
         }
     })
 
-        os.system("cls")
-        print(f"{Fore.LIGHTBLUE_EX}Starting generating art.")
+    os.system("cls")
+    print(f"{Fore.LIGHTBLUE_EX}Starting generating art.")
 
-        createArt = {}
-        createArt["state"] = "generating"
-        while createArt["state"] == "generating":
-            createArt = requests.get(f"https://app.wombo.art/api/tasks/{taskID}", headers={
+    createArt = {}
+    createArt["state"] = "generating"
+    while createArt["state"] == "generating":
+        createArt = requests.get(f"https://app.wombo.art/api/tasks/{taskID}", headers={
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0",
             "Accept": "*/*",
             "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -104,30 +99,30 @@ if st.button("Get Art"):
             "TE": "trailers"
         }).json()
 
-            os.system("cls")
-            print(f"{Fore.LIGHTYELLOW_EX}Art is still generating.")
-
-        artURL = createArt["result"]["final"]
-
-        if not os.path.isdir("results/"):
-            os.makedirs("results/")
-    
-        with open(f"results/{taskID}.png", "wb") as file:
-            file.write(requests.get(artURL).content)
-            file.close()
-
-        os.system(f"explorer {os.getcwd()}\\results\{taskID}.png")
-
         os.system("cls")
-        print(f"{Fore.LIGHTGREEN_EX}Art generated with success! {artURL}")
+        print(f"{Fore.LIGHTYELLOW_EX}Art is still generating.")
 
-        input()
+    artURL = createArt["result"]["final"]
 
-    if __name__ == "__main__":
-        try:
-            os.system("cls")
-            title = input(f"{Fore.LIGHTYELLOW_EX}Please enter your art title.\n\n{Fore.RESET}~# ")
+    if not os.path.isdir("results/"):
+        os.makedirs("results/")
+    
+    with open(f"results/{taskID}.png", "wb") as file:
+        file.write(requests.get(artURL).content)
+        file.close()
 
-            generate(title)
-        except KeyboardInterrupt:
-            exit()
+    os.system(f"explorer {os.getcwd()}\\results\{taskID}.png")
+
+    os.system("cls")
+    print(f"{Fore.LIGHTGREEN_EX}Art generated with success! {artURL}")
+
+    input()
+
+if __name__ == "__main__":
+    try:
+        os.system("cls")
+        title = input(f"{Fore.LIGHTYELLOW_EX}Please enter your art title.\n\n{Fore.RESET}~# ")
+
+        generate(title)
+    except KeyboardInterrupt:
+        exit()
