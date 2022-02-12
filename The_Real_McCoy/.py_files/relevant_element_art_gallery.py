6115761ba6ca287edb17aab56f128eb0268ed2f4
Airmanf7 <88789678+Airmanf7@users.noreply.github.com>
2022-02-12 08:50:29 -0500

    create art generator

--- The_Real_McCoy/.py_files/relevant_element_art_gallery.py
+++ The_Real_McCoy/.py_files/relevant_element_art_gallery.py
@@ -1,20 +1,49 @@
+
+
 from PIL import Image
 import glob
 import streamlit as st
+from itertools import cycle
+import time
+from multiapp import MultiApp
+from apps import art_gallery
+
+relevant_element = MultiApp()
+
+def app():
+    st.title('Art Gallery')
+
+    st.write('Enjoy this art gallery!')
+
+relevant_element.add_app("Home", art_gallery.app)
 
 
-st.title('Art Gallery')
 
-st.write('Enjoy this art gallery!')
+# The main app
+relevant_element.run()
+    
+Images = ["C://Users//Airma//FinTechClass//Project_3_TheRelevantElement//The_Real_McCoy//resources//Images//Dream.jpg",
+          "C://Users//Airma//FinTechClass//Project_3_TheRelevantElement//The_Real_McCoy//resources//Images//love.jpg",
+         ]
 
-image = Image.open("C://Users//Airma//FinTechClass//Project_3_TheRelevantElement//The_Real_McCoy//resources//Images//Dream.jpg")
-
-st.image(image, caption="Dream", use_column_width=True)
+caption = ["Dream", "Love"] 
 
 
-# images = []
-# for filename in glob.glob("C://Users//Airma//FinTechClass//Project_3_TheRelevantElement//The_Real_McCoy//resources//Images"):
-#     images.append(Image.open(filename))
+# index = 0 
+# for image in Images:
+#         im = Image.open(image)
+#         st.image(im, caption=caption[index], width=350)
+#         time.sleep(0)
+#         index += 1 
 
-# image = Image.open(images)
-# st.image(image, use_column_width=True)
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("A cat")
+    st.image(Images[0], caption=caption[0], width=350)
+
+with col2:
+    st.header("A dog")
+    st.image(Images[1], caption=caption[1], width=350)
+
+
