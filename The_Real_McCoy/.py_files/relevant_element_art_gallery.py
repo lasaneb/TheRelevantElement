cac7b9c8a12a3e27d634dbf6f7df8e3eb89afeb1
Airmanf7 <88789678+Airmanf7@users.noreply.github.com>
2022-02-17 21:16:32 -0500

    adding pictures

--- The_Real_McCoy/.py_files/relevant_element_art_gallery.py
+++ The_Real_McCoy/.py_files/relevant_element_art_gallery.py
@@ -5,7 +5,7 @@ import streamlit as st
 from itertools import cycle
 import time
 from multiapp import MultiApp
-from apps import art_gallery
+from apps import art_gallery, wombo_art_generator
 
 relevant_element = MultiApp()
 
@@ -15,36 +15,17 @@ def app():
     st.write('Enjoy this art gallery!')
 
 relevant_element.add_app("Home", art_gallery.app)
-relevant_element.add_app("Generate Art with Wombo", wombo_tool.generate)
+relevant_element.add_app("Generate Art with Wombo", wombo_art_generator.app)
 
 
 # The main app
 relevant_element.run()
     
-Images = ["C://Users//Airma//FinTechClass//Project_3_TheRelevantElement//The_Real_McCoy//resources//Images//Dream.jpg",
-          "C://Users//Airma//FinTechClass//Project_3_TheRelevantElement//The_Real_McCoy//resources//Images//love.jpg"
-         ]
-
-caption = ["Dream", "Love"] 
-
-st.image(Images[0], width=350, caption=caption[0])
 
 
-# index = 0 
-# for image in Images:
-#         im = Image.open(image)
-#         st.image(im, caption=caption[index], width=350)
-#         time.sleep(0)
-#         index += 1 
 
-# col1, col2 = st.columns(2)
 
-# with col1:
-#     st.header("A cat")
-#     st.image(Images[0], caption=caption[0], width=350)
 
-# with col2:
-#     st.header("A dog")
-#     st.image(Images[1], caption=caption[1], width=350)
+
 
 
